190fbd3f8dc762b44ed2619c5411f3baa0a56de6
alvarobc <alvaro.blazquezcheca@zooplus.com>
2020-10-18 17:15:56 +0200

    Remove security group and template

--- main.tf
+++ main.tf
@@ -14,22 +14,6 @@ module "http_80_security_group" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-module "security-group_ssh" {
-  source              = "terraform-aws-modules/security-group/aws//modules/ssh"
-  version             = "3.16.0"
-  name                = "SSH-Access"
-  vpc_id              = data.aws_vpc.default_vpc.id
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_launch_template" "launch_template" {
-  name                   = "ec2-t2micro-amazonlinux2"
-  image_id               = var.amazon_linux_2_ami_id
-  instance_type          = "t2.micro"
-  key_name               = "Frankfurt-kp"
-  vpc_security_group_ids = tolist([module.http_80_security_group.this_security_group_id])
-}
-
 module "ec2_instance" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "2.15.0"
@@ -38,6 +22,6 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   key_name               = "Frankfurt-kp"
   subnet_id              = sort(data.aws_subnet_ids.vpc_subnets.ids)[0]
-  vpc_security_group_ids = [module.security-group_ssh.this_security_group_id, module.http_80_security_group.this_security_group_id]
+  vpc_security_group_ids = [module.http_80_security_group.this_security_group_id]
   user_data = file("./shell/http-server.sh")
 }
